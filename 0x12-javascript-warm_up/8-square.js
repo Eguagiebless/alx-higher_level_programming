276e10f0d5237394637a5f55a208c1b150ec66a9
Eguagiebless <eguagiebless3@gmail.com>
2024-07-12 03:29:07 -0700

    print_a_square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const size = isNaN(parseInt(process.argv[1])) ? "Missing size" : String.repeat("X", parseInt(process.argv[1]));
+const size = isNaN(parseInt(process.argv[2])) ? 'Missing size' : String.repeat('X', parseInt(process.argv[2]));
 
-console.log(size.split("\n").join("\n"));
+console.log(size.split('\n').join('\n'));
