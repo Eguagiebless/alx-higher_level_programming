c24863af646e2d600fdb118c6fa50108cbc0fa39
Eguagiebless <eguagiebless3@gmail.com>
2024-07-12 08:18:29 -0700

    change format

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,17 @@
-
 #!/usr/bin/node
-const firstArg = process.argv[2];
+const x = process.argv[2];
 
-if (isNaN(parseInt (firstArg))) {
+if (!parseInt(x)) {
   console.log('Missing size');
 } else {
-  const size = parseInt (firstArg);
-  let square = '';
+  for (let i = 0; i < x; i++) {
+    let y = 0;
+    let myVar = '';
 
-  for (let i = 0; i < size; i++) {
-    square += 'X'.repeat(size) + '\n';
+    while (y < x) {
+      myVar = myVar + 'X';
+      y++;
+    }
+    console.log(myVar);
   }
-  console.log(square.slice(0, -1));
 }
