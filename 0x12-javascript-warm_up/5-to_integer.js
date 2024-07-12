46e6a7b522bf1896873a5e64a8fb9e89275e5d9d
Eguagiebless <eguagiebless3@gmail.com>
2024-07-12 02:41:31 -0700

    removed_double_quote

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const firstArg = process.argv[2];
 if (Number.isInteger(Number(firstArg))) {
   console.log(`My number: ${firstArg}`);
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
