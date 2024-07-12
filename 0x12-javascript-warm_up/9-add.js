73dc2c6188a21337ce3ee751b9b392f6974200eb
Eguagiebless <eguagiebless3@gmail.com>
2024-07-12 04:45:39 -0700

    removed-quotes2

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,6 +2,8 @@
 function add(a, b) {
   return isNaN(a) || isNaN(b) ? 'Error: Not a number' : a + b;
 }
+
 const firstArg = process.argv[2];
 const secondArg = process.argv[3];
+
 console.log(add(firstArg, secondArg));
