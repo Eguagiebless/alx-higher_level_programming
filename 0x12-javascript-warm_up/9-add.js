b100838a2936aa87a7bcdc7489f71149a0535911
Eguagiebless <eguagiebless3@gmail.com>
2024-07-12 04:17:13 -0700

    prints_addition_of_2 intergers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,7 @@
 #!/usr/bin/bash
 function add(a, b) {
-  return a + b;
+  return isNaN(a) || isNaN(b) ? 'Error: Not a number' : a + b;
 }
-
-const firstArg = parseInt(process.argv[2]);
-const secondArg = parseInt(process.argv[3]);
-
-if (isNaN(firstArg) || isNaN(secondArg)) {
-  console.log('Error: Please provide two numbers');
-} else {
-  const sum = add(firstArg, secondArg);
-  console.log(`The sum of ${firstArg} and ${secondArg} is ${sum}`);
-}
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+console.log(add(firstArg, secondArg));
