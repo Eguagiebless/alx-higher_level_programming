6f00f41210b462f5d6939109934b013edfe510c4
Eguagiebless <eguagiebless3@gmail.com>
2024-07-12 03:48:37 -0700

    fixed_quote_error

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,10 +2,10 @@
 #!/usr/bin/node
 const firstArg = process.argv[2];
 
-if (isNaN(parseInt(firstArg))) {
+if (isNaN(parseInt (firstArg))) {
   console.log('Missing size');
 } else {
-  const size = parseInt(firstArg);
+  const size = parseInt (firstArg);
   let square = '';
 
   for (let i = 0; i < size; i++) {
