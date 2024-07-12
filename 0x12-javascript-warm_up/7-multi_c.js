1afe9ce32e98f78bb2e3fa7c097d7f21c01dbc64
Eguagiebless <eguagiebless3@gmail.com>
2024-07-12 03:10:29 -0700

    fixed-error

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,9 @@
-#!/usr/bin/node 
-const firstArg = process.argv[2];
+t x = process.argv[2];
 
-if (isNaN(parseInt(firstArg))) {
-  console.log('Missing number of occurrences');
+if (!parseInt(x)) {
+	  console.log('Missing number of occurrences');
 } else {
-  const numOccurrences = parseInt(firstArg);
-  for (let i = 0; i < numOccurrences; i++) {
-  console.log('C is fun');
-  }
+	  for (let i = 0; i < x; i++) {
+		      console.log('C is fun');
+		    }
 }
